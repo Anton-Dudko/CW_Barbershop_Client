1994049ef82395f463b0b94f61de2bb97758121c
Anton-Dudko <60478086+Anton-Dudko@users.noreply.github.com>
2022-03-14 12:18:46 +0300

    with tests

---
+++ src/test/java/net/javaguides/springboot/springsecurity/controller/HaircutControllerTest.java
@@ -0,0 +1,75 @@
+package net.javaguides.springboot.springsecurity.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HaircutControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    long id = 2L;
+    String name = "jpg";
+
+    @BeforeEach
+    void printApplicationContext() {
+        Arrays.stream(applicationContext.getBeanDefinitionNames())
+                .map(name -> applicationContext.getBean(name).getClass().getName())
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testAllFiles() throws Exception {
+        mockMvc.perform(get("/hair-cut"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testDeleteFile() throws Exception {
+        mockMvc.perform(delete("/hair-cut/" + id))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testGetFile() throws Exception {
+        mockMvc.perform(get("/hair-cut/" + id))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+
+
+}
---
+++ src/test/java/net/javaguides/springboot/springsecurity/controller/OrderControllerTest.java
@@ -0,0 +1,55 @@
+package net.javaguides.springboot.springsecurity.controller;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderControllerTest extends TestCase {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    private long idOrders = 2L;
+
+    @BeforeEach
+    void printApplicationContext() {
+        Arrays.stream(applicationContext.getBeanDefinitionNames())
+                .map(name -> applicationContext.getBean(name).getClass().getName())
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testSaveHaircut() throws Exception {
+
+        mockMvc.perform(post("/orders" + idOrders))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testGetHaircut() throws Exception {
+        mockMvc.perform(get("/orders" + idOrders))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+}
